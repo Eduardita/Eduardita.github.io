7cd9f957c92c77e5f26a8a719aa4c8c677af80b8
Eduardita <editapaz@gmail.com>
2016-07-14 15:50:11 -0400

    nuevo proyecto, monito que saluda

--- sketch.js
+++ sketch.js
@@ -1,102 +1,99 @@
-//12-07-2016 taller p5 número 4
-//elipse que sigue al mouse en fondo verde
-// comand + T arregla formato
+var largoBrazo = 180;
+var anchoBrazo = 10;
+var a = 0
+var b = 0
+var xBrazo = 340
+var yBrazo = 160
 
-//tenemos variables y funciones
-//para las variables tenemos; declaracion y asignacion, y fuera de las funciones
-//declaracion ------- var ----- no declarar 2 veces 
-var anchoElipse;
-// asignación
-anchoElipse = 100;
+//dibujo el monito
+function monito() {
 
-// pasan cosas y se reasigna
-anchoElipse = 50;
-
-//manera abreviada de declarar y asignar
-var alturaElipse = 100;
-//reasignar
-alturaElipse = 50;
-
-//scope, rango o alcance
-//hay variables globales y locales
-//variables globales son declaradas fuera de los cuerpos de las funciones
-//las locales van declaradas dentro de los cuerpos de las funciones, entre {}
-//no pueden ser llamadas o variadas desde otra funcion
-
-
-//declaracion de funcion setup
-function setup() {
-
-  //frameRate (frecuencia de repeticion de draw, frames x seg) ------> por defecto es 60
-  frameRate(30)
-    //createCanvas (dimhori, dim vertical) en pixeles;
-    //createCanvas(600, 400);
-  createCanvas(windowWidth, windowHeight);
-  //background(r, g, b) de 0 a 255;
-  background(0, 255, 0);
-
-  //relleno de la elipse
-  //fill(r, g, b, transparencia) ----->valores de 0 a 255
-
-  fill(0, 0, 255);
-
-  //color borde ---- > stroke(r,g,b)
-  //no borde 
-  //noStroke
-
-
-
-  //strokeWeight(px) -----> anchoborde
+  fill(0, 200, 0)
+  strokeWeight(3);
+  rect(0, 350, width, 250)
+  fill(0, 40, 200, 50)
+  rect(0, 0, width, 350)
+  fill(255, b, 0, 100);
+  stroke(0);
   strokeWeight(5);
+  arc(460, 160, largoBrazo, anchoBrazo, 185, 175, CHORD)
+  ellipse(400, 100, 100, 100);
+  triangle(400, 150, 500, 300, 300, 300)
+  rect(355, 300, 15, 80, 0, 0, 10, 10)
+  rect(mouseX, mouseY, 15, 80, 0, 0, 10, 10)
+
 
 
 }
 
-//declaracion de funcion draw
+
+
+
+function setup() {
+  frameRate(40)
+  angleMode(DEGREES)
+
+  createCanvas(600, 600);
+
+
+}
+
 function draw() {
-  //ejecutar funcion fondoRojo
-  //fondoRojo();
-  //ellipse(posx, posy, ancho, alto);
-  stroke(255, 255, 000);
-  fill(255, 0, 0, 100);
-  //ellipse(mouseX, mouseY, anchoElipse, alturaElipse);
-  //escribe en la consola de javascript
-  console.log("hola que tal?");
-  console.log(alturaElipse);
-  muchasElipses();
-  //screenshot savecanvas (nombre, extension)
-  //saveCanvas("mycanvas", "jpg")
 
-}
+  background(255)
+  monito();
 
-//declaracion de funcion nueva , parentesis van si o si, aunque no lleven nada
-//function nombreFuncion(argumento 1, arg2, etc) {
-//}
-//declaracion funcion rojo
+  translate(380, 160);
 
-function fondoRojo() {
-  //declaracion y asignacion de variable local llamada rojo
-  var rojo = 255;
-  //usamos variable local llamada rojo
-  background(rojo, 0, 0);
-  //imprimir variable local en consola
-  console.log("el valor de la variable rojo es " + rojo + " salud");
-}
-
-function muchasElipses() {
-  var muchas = 100;
-
-  //for(variable de loop; condicion de seguir; refrescar){}
-  //i++ = i+1
-  for (var i = 0; i < muchas; i++) {
-    fill(random(255), random(255), random(255), random(255));
-    noStroke();
-    var ancho = 100;
-    var alto = 100;
-    //funcion random(x) arroja valor aleatorio entre 0 y X
-    ellipse(random(width), random(height), random(ancho), random(alto));
-
-
+  rotate(a);
+  arc(-40, 0, largoBrazo, anchoBrazo, 5, 355, CHORD)
+  a = a + 1;
+  if (a > 60) {
+    b = b + 50
+    a = 0
+  }
+  if (b > 250) {
+    b = 0
   }
 
+}
+
+//function angulo() {
+//frameRate (20)
+//for (var i = 0; i < 30; i++)
+//rotate(a);
+//arc(0, 0, largoBrazo, anchoBrazo, 5, 355, CHORD)
+//a = a + 1;
+//if (a > 50) {
+//  anguloB()
+//}
+
+function anguloB() {
+
+  for (var i = 0; i < 50; i++) {
+
+    background(255)
+    monito();
+    translate(340, 160);
+
+    rotate(a);
+    arc(0, 0, largoBrazo, anchoBrazo, 5, 355, CHORD)
+    a = a - 1;
+  }
+}
+
+
+//if (a = 50) {
+for (var i = 0; i < 50; i++) {
+
+  background(255)
+  rotate(-a + 1)
+  translate(0, 0)
+  monito();
+  translate(340, 160);
+
+  rotate(a);
+  arc(0, 0, largoBrazo, anchoBrazo, 5, 355, CHORD)
+  a = a - 1;
+  //}
 }
