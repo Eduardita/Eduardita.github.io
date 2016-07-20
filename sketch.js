bc9bd23d02183f06a3f58594428d7dbe8257d155
Eduardita <editapaz@gmail.com>
2016-07-20 17:17:52 -0300

    moviendo cabeza con mouse

--- sketch.js
+++ sketch.js
@@ -4,36 +4,48 @@ var a = 0
 var b = 0
 var xBrazo = 340
 var yBrazo = 160
+var xcara = 400
+var ycara = 100
 
-//dibujo el monito
+
 function monito() {
 
-  fill(0, 200, 0)
-  strokeWeight(3);
-  rect(0, 350, width, 250)
-  fill(0, 40, 200, 50)
-  rect(0, 0, width, 350)
+
+  //monito
   fill(255, b, 0, 100);
   stroke(0);
   strokeWeight(5);
   arc(460, 160, largoBrazo, anchoBrazo, 185, 175, CHORD)
-  ellipse(400, 100, 100, 100);
+  ellipse(xcara, ycara, 100, 100);
   triangle(400, 150, 500, 300, 300, 300)
   rect(355, 300, 15, 80, 0, 0, 10, 10)
-  rect(mouseX, mouseY, 15, 80, 0, 0, 10, 10)
+  rect(430, 300, 15, 80, 0, 0, 10, 10)
 
-
+  var cara = dist(xcara, ycara, mouseX, mouseY)
+  if (cara < 50 && mouseIsPressed) {
+    xcara = mouseX;
+    ycara = mouseY;
+  } else {
+    xcara = 400
+    ycara = 100;
+  }
 
 }
 
 
 
 
+
+
 function setup() {
-  frameRate(40)
+
   angleMode(DEGREES)
 
   createCanvas(600, 600);
+
+
+
+
 
 
 }
@@ -41,59 +53,63 @@ function setup() {
 function draw() {
 
   background(255)
+  beginShape();
+
+  //casita
+  strokeWeight(5)
+  noFill();
+  vertex(50, 350);
+  vertex(50, 250)
+  vertex(115, 180)
+  vertex(180, 250)
+  vertex(180, 350);
+  endShape();
+
+
+
+  suelo();
+
+  cielo();
+
   monito();
 
-  translate(380, 160);
 
+  //que el brazo salude ..... no logro hacer que baje luego de los 60 grados
+  translate(380, 160);
+  a += 0.5
   rotate(a);
   arc(-40, 0, largoBrazo, anchoBrazo, 5, 355, CHORD)
-  a = a + 1;
-  if (a > 60) {
+
+  if (a == 60) {
     b = b + 50
-    a = 0
+    a = a = 0
+
   }
+
   if (b > 250) {
     b = 0
   }
 
 }
+// suelo
+function suelo() {
+  var f = 0
+  for (var i = 350; i < height; i += 1) {
 
-//function angulo() {
-//frameRate (20)
-//for (var i = 0; i < 30; i++)
-//rotate(a);
-//arc(0, 0, largoBrazo, anchoBrazo, 5, 355, CHORD)
-//a = a + 1;
-//if (a > 50) {
-//  anguloB()
-//}
-
-function anguloB() {
-
-  for (var i = 0; i < 50; i++) {
-
-    background(255)
-    monito();
-    translate(340, 160);
-
-    rotate(a);
-    arc(0, 0, largoBrazo, anchoBrazo, 5, 355, CHORD)
-    a = a - 1;
+    strokeWeight(3);
+    stroke(0, f, 0, 150)
+    line(0, i, width, i);
+    f = f + 0.5
   }
 }
 
+function cielo() {
+  var c = 0
+  for (var e = 350; 0 < e; e--) {
 
-//if (a = 50) {
-for (var i = 0; i < 50; i++) {
-
-  background(255)
-  rotate(-a + 1)
-  translate(0, 0)
-  monito();
-  translate(340, 160);
-
-  rotate(a);
-  arc(0, 0, largoBrazo, anchoBrazo, 5, 355, CHORD)
-  a = a - 1;
-  //}
+    strokeWeight(1);
+    stroke(20, 20, c, 200);
+    line(0, e, width, e)
+    c += 0.3
+  }
 }
